a7d579663d078aa298ac6fa459eb5d6636dcb407
DeckerSU <support@decker.su>
2016-12-20 12:35:20 +0200

    some changes to build with mod framework_av

--- prebuilt/libs/Android.mk
+++ prebuilt/libs/Android.mk
@@ -22,6 +22,37 @@ LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/Camera/Camera.apk
 LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE        := Camu
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := APPS
+LOCAL_CERTIFICATE   := platform
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/Camu/Camu_1.4.apk
+LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_EXPORT_C_INCLUDE_DIRS = libdpframework/include
+LOCAL_SRC_FILES = $(PROPRIETARY_PATH)/lib/libdpframework.so 
+LOCAL_MULTILIB := 32
+include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE        := EngineerMode
+#LOCAL_MODULE_TAGS   := optional
+#LOCAL_MODULE_CLASS  := APPS
+#LOCAL_CERTIFICATE   := platform
+#LOCAL_MODULE_SUFFIX := .apk
+#LOCAL_SRC_FILES     := $(PROPRIETARY_PATH)/app/EngineerMode/EngineerMode.apk
+#LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app
+#include $(BUILD_PREBUILT)
+
+
 #include $(CLEAR_VARS)
 #LOCAL_MODULE        := libDocVfbEngineLib_m81
 #LOCAL_MODULE_TAGS   := optional
